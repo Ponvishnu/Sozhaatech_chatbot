01a4c97d9bb28d425f0864c1921e92937284ca59
Ponvishnu <147022646+Ponvishnu@users.noreply.github.com>
2025-09-17 07:43:21 -0700

    backend.py

--- backend.py
+++ backend.py
@@ -25,7 +25,7 @@ COMPANY_EMAIL = "groupsozhaa@gmail.com"
 SENDGRID_API_KEY = "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"
 
 # WhatsApp Cloud API
-WHATSAPP_TOKEN = "EAAQYRWtYvBoBPWx91g2TtbgYmePdFDt8uqvA4CG71Qm2HrpQQEEfCk1F9vACT0SH1cx0ZAMfuCtYINu3LZA6Cnhe2WVeUj5lce04GJlvtZBMlTcgGyGm4w4fwZAM2SU44OSpWhPoYr5WNOJUZBDN2QfVSsuCMhkZCJbaa0JIZB2nw3XCub5mzM5yP0wbWqcpmJjqadeyMnZBLxOChQVjr5H4kltuhn70ZBhqEZB1XqKVDUXgZDZD"
+WHATSAPP_TOKEN = "EAAQYRWtYvBoBPSZBXZAUkBh9SI8ROdTDjEYawb5nXX4IZC8DWJSV5Xn2aOHakiG31tZABNZB1ffHHhRwlIZCyqblObBNVeU0Aly9vYUJbZBJDUI62ZA4SWgGhkqMo6RXe55EkoZCZC14mRsFIOV1PlsuP79vfZAjXAXZCaKSxhPJcLKXNoIkM7ZB1ZBOtXBLNuCF5lMAZDZD"
 WHATSAPP_PHONE_NUMBER_ID = "760558620282792"
 COMPANY_WA_NUMBER = "+917094062522"
 GRAPH_API_BASE = "https://graph.facebook.com/v22.0"
@@ -334,6 +334,7 @@ async def chat_endpoint(payload: ChatPayload, background_tasks: BackgroundTasks)
 
     background_tasks.add_task(save_and_email)
     return {"reply": assistant_text}
+
 
 
 
